45a2b675e6d9a3e9b79efeb7c961a50f731f28d0
oyron <oyron@statoil.com>
2018-10-05 15:09:19 +0200

    Bugfix in Reveal.js (part of Hacktoberfest)

--- plugin/notes/notes.js
+++ plugin/notes/notes.js
@@ -9,9 +9,17 @@
  * 3. This window proceeds to send the current presentation state
  *    to the notes window
  */
+
 var RevealNotes = (function() {
 
+    var notesPopup = null;
+
 	function openNotes( notesFilePath ) {
+
+        if (notesPopup && !notesPopup.closed) {
+            notesPopup.focus();
+            return;
+        }
 
 		if( !notesFilePath ) {
 			var jsFileLocation = document.querySelector('script[src$="notes.js"]').src;  // this js file path
@@ -19,7 +27,7 @@ var RevealNotes = (function() {
 			notesFilePath = jsFileLocation + 'notes.html';
 		}
 
-		var notesPopup = window.open( notesFilePath, 'reveal.js - Notes', 'width=1100,height=700' );
+		notesPopup = window.open( notesFilePath, 'reveal.js - Notes', 'width=1100,height=700' );
 
 		if( !notesPopup ) {
 			alert( 'Speaker view popup failed to open. Please make sure popups are allowed and reopen the speaker view.' );
